c25290a26043bed47f0418d10a590f493c8afb39
Jhon <jhon-43769@outlook.com>
2024-04-28 12:46:09 -0400

    Actualizar Blockchain.js con validación de cadena y función de impresión

--- src/blockchain.js
+++ src/blockchain.js
@@ -1,5 +1,5 @@
 const Block = require('./block');
-
+const SHA256 = require('crypto-js/sha256');
 class Blockchain {
     constructor() {
         this.chain = [];
@@ -16,12 +16,15 @@ class Blockchain {
 
     addBlock(block){
         let self = this;
-        return new Promise((resolve, reject) => {
+        return new Promise(async (resolve, reject) => {
             block.height = self.chain.length;
             block.time = new Date().getTime().toString();
-
             if(self.chain.length > 0) {
                 block.previousBlockHash = self.chain[self.chain.length - 1].hash;
+            }
+            let errros = await self.validateChain();
+            if(errros.length > 0) {
+                reject(new Error('The chain is not valid: ', errros));
             }
             block.hash = SHA256(JSON.stringify(block)).toString();
             self.chain.push(block);
@@ -29,8 +32,33 @@ class Blockchain {
         });
     }
 
+    validateChain() {
+        let self = this;
+        const errros = [];
+        
+
+        return new Promise(async (resolve, reject) => {
+            self.chain.map(async(block) => {
+                try {
+                    let isValid = await block.validate();
+                    if(!isValid) {
+                        errros.push(new Error(`Block ${block.height} is not valid`));
+                    }
+                } catch (err) {
+                    reject(err);
+                }
+            });
+            resolve(errros);
+        });
+    }
 
 
+    print() {
+        let self = this;
+        for (let block of self.chain) {
+            console.log(block.toString());
+        }
+    }
 }
 
 
